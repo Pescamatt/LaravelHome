e99db3d9e00ed27bb01f75a100b738948112bce4
Pescamatt <pescador.mattia@gmail.com>
2024-10-17 20:48:28 +0200

    start lesson 6

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,4 +1,8 @@
 <x-layout>
     <x-slot:heading>Home Page</x-slot:heading>
     <h1>Hello this is the Home page</h1>
+
+    @foreach($jobs as $job)
+        <li>{{ $job['title'] }}</li>
+    @endforeach
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('homepage');
+    return view('homepage', [
+        'jobs' => [
+            [
+                'title' => 'Director',
+                'salary' => '50.000'
+            ],
+            [
+                'title' => 'Programmer',
+                'salary' => '20.000'
+            ],
+            [
+                'title' => 'Teacher',
+                'salary' => '40.000'
+            ]
+        ]
+    ]);
 });
 
 Route::get('/about', function () {
