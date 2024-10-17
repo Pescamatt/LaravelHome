973829ba8019396088462231b455f42f83fece0d
Pescamatt <pescador.mattia@gmail.com>
2024-10-17 11:29:50 +0200

    second commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:heading>About Page</x-slot:heading>
+    <h1>Hello this is the About</h1>
+</x-layout>
---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:heading>Home Page</x-slot:heading>
+    <h1>Hello this is the Home page</h1>
+</x-layout>
